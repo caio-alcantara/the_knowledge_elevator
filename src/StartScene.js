49eccea81f126f3360b4b51cc5a4e4194d980b38
caio.santos <caioalcantarasantos3@gmail.com>
2024-02-16 08:12:35 -0300

    Adiciona mais comentarios

--- src/StartScene.js
+++ src/StartScene.js
@@ -1,4 +1,4 @@
-var backgorund, predio, porta, nuvem1, nuvem2, passaro1, passaro2, logo, texto, mensagem, index, musica;
+var backgorund, predio, porta, nuvem1, nuvem2, passaro1, passaro2, logo, texto, mensagem, messageIndex, musica, whiteScreen;
 
 class StartScene extends Phaser.Scene {
     constructor() {
@@ -10,6 +10,7 @@ class StartScene extends Phaser.Scene {
         this.load.image('predio', '../assets/predio_sem_porta.png');
         this.load.image('porta', '../assets/porta_fechada.png');
         this.load.image('logo', '../assets/logoFinal.png');
+
         // Nuvens
         this.load.image('nuvem1', '../assets/nuvem1.png');
         this.load.image('nuvem2', '../assets/nuvem2.png');
@@ -33,32 +34,36 @@ class StartScene extends Phaser.Scene {
         predio = this.add.image(395, 270, 'predio').setScale(0.4);
         porta = this.add.image(395, 420, 'porta').setScale(0.15);
         logo = this.add.image(400, 200, 'logo').setScale(0.35);
-        logo.setAlpha(0);
+        logo.setAlpha(0); // Deixa o logo invisível para fazer o efeito fade in
         nuvem1 = this.add.image(640, 240, 'nuvem1').setScale(0.35);
         nuvem2 = this.add.image(200, 80, 'nuvem2').setScale(0.22);
         // var nuvem3 = this.add.image(400, 250, 'nuvem3').setScale(0.3);
         // var nuvem4 = this.add.image(600, 300, 'nuvem4').setScale(0.3);
 
+        // Adicione um texto para exibir a mensagem de início
         mensagem = "Aperte em qualquer lugar para iniciar!";
-        index = 0;
+        messageIndex = 0;
         texto = this.add.text(220, 565, '', { fontFamily: 'Trebuchet MS', fontSize: 20, color: '#ffffff' });
 
-        var blackScreen = this.add.rectangle(0, 0, config.width, config.height, 0xFFFFFF);
-        blackScreen.setOrigin(0, 0);
+        // Adicione um retângulo branco para fazer o fade in da tela toda
+        whiteScreen = this.add.rectangle(0, 0, config.width, config.height, 0xFFFFFF);
+        whiteScreen.setOrigin(0, 0);
 
-    // Adicione um tween para fazer o fade in
+       // Método tweens faz animações, tais como o fade in e o bounce da logo
         this.tweens.add({
             delay: 300,
-            targets: blackScreen,
+            targets: whiteScreen,
             alpha: 0, // Defina o valor final de opacidade desejado (0 para totalmente transparente)
             duration: 3000, // Duração em milissegundos
             onComplete: function () {
                 // Código a ser executado após o fade in ser concluído
-                blackScreen.destroy(); // Remova o retângulo preto se necessário
+                whiteScreen.destroy(); // Remova o retângulo preto se necessário
             }
         });
 
-        
+        // Efeito de digitação das letras na tela
+        // É feito, basicamente, com um cronômetro que adiciona uma letra da variavel mensagem à
+        // variável texto a cada 100ms
         this.time.addEvent({
             delay: 4000,  // Atraso em milissegundos antes de começar
             callback: function () {
@@ -67,14 +72,18 @@ class StartScene extends Phaser.Scene {
                     delay: 100,  // Tempo em milissegundos entre cada letra
                     repeat: mensagem.length - 1,
                     callback: function () {
-                        texto.text += mensagem[index];
-                        index++;
+                        texto.text += mensagem[messageIndex];
+                        messageIndex++;
                     },
                 });
             },
             callbackScope: this
         });
 
+
+        // Animações dos pássaros
+        // Foram feitos dois pássaros para que um ficasse com o frameRate mais rápido que o outro, dando a impressão
+        // de que estão voando em velocidades diferentes
         passaro1 = this.add.sprite(100, 300, 'passaro').setScale(0.8);
         passaro2 = this.add.sprite(600, 150, 'passaro').setScale(0.4);
         this.anims.create({
@@ -93,16 +102,19 @@ class StartScene extends Phaser.Scene {
         passaro1.anims.play('fly1', true);
         passaro2.anims.play('fly2', true);
 
+        // Definindo a profundidade dos elementos, o que cria o efeito
+        // de que alguns elementos estão na frente de outros. Ex.: A nuvem passando por trás do prédio
         predio.setDepth(2);
         porta.setDepth(3);
         nuvem1.setDepth(1);
         nuvem2.setDepth(1);
         passaro1.setDepth(3);
         logo.setDepth(3);
-        blackScreen.setDepth(4);
+        whiteScreen.setDepth(4);
         // nuvem3.setDepth(1);
         // nuvem4.setDepth(1);
 
+        // Efeito de bounce da logo
         this.tweens.add({
             targets: logo,
             y: '+=40',
@@ -134,11 +146,9 @@ class StartScene extends Phaser.Scene {
         }
         if (passaro2.x < -100) {
             passaro2.x = 900;
-        }
-
-
-        
-
+        } 
     }
     
 }
+
+// Temos que adicionar uma função de onclick() para que o jogo possa avançar para a próxima cena
